1896b18bc7cbce03f658b6df12db4aa7b831a901
Vladik <qvviko@gmail.com>
2019-11-13 21:19:05 +0300

    ! remove tags from messages

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -4,35 +4,34 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.log4j.Level
+import org.apache.spark.sql.{Column, SparkSession}
+import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.regexp_replace
 
 object MainClass{
   def main(args: Array[String]){
+
     val spark = SparkSession.builder
-      .master("local[4]")
-      .appName("Main")
+      .master("local")
+      .appName("Spark CSV Reader")
       .getOrCreate
-    val df = spark.read
+
+    var df = spark.read
       .format("csv")
-      .option("header", "true") //first line in file has headers
+      .option("header", "true")
       .load("dataset/train.csv")
-    val model = new word2vec()
-    model.train(df)
-    // Configure Twitter credentials using twitter.txt
-//    setupTwitter()
-//    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
-//    setupLogging()
-//
-//    // Create a DStream from Twitter using our streaming context
-//    val tweets = TwitterUtils.createStream(ssc, None)
-//
-//    // Now extract the text of each status update into RDD's using map()
-//    val statuses = tweets.map(status => status.getText())
-//    statuses.print()
-//
-//    ssc.start()
-//    ssc.awaitTermination()
+    df = df.withColumn("SentimentText", regexp_replace(df("SentimentText"), "@[a-zA-Z0-9_]+", "") )
 
+    val tokenizer = new RegexTokenizer()
+      .setInputCol("SentimentText")
+      .setOutputCol("Tokens")
+      .setPattern("\\W+")
+      .setGaps(true)
+
+    val tmp = tokenizer.transform(df)
+
+    val model = new word2vec().train(tmp, 1)
+    val result = model.transform(tmp)
   }
-
 }
--- src/main/scala/model/word2vec.scala
+++ src/main/scala/model/word2vec.scala
@@ -4,24 +4,19 @@ import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer, Word2Vec, Word2Ve
 import org.apache.spark.sql.{SQLContext, SparkSession, DataFrame}
 
 class word2vec {
-
-  val model: Word2Vec = new Word2Vec().setMaxIter(5).setVectorSize(128).setInputCol("Tokens").setOutputCol("Result")
   var trained_model: Word2VecModel = _
 
-  def train(df: DataFrame): Word2VecModel = {
-    val tokenizer = new RegexTokenizer()
-      .setInputCol("SentimentText")
-      .setOutputCol("Tokens")
-      .setPattern("\\W+")
-      .setGaps(true)
-    val tmp = tokenizer.transform(df)
-    tmp.show(10)
-    trained_model = model.fit(tmp)
-    val result = trained_model.transform(tmp)
-    result.select("result").take(3).foreach(println)
+  def train(dataset: DataFrame, maxIter: Int): Word2VecModel = {
+
+
+    val model = new Word2Vec()
+      .setMaxIter(maxIter)
+      .setVectorSize(128)
+      .setInputCol("Tokens")
+      .setOutputCol("result")
+    trained_model = model.fit(dataset)
     trained_model
   }
-
   def save_model(path:String): Unit = {
     trained_model.save(path)
   }
