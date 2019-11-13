42a77943033406d0238a326a59f92aae4c2c029b
Farit Galeev <f.galeev@innopolis.ru>
2019-11-13 18:19:04 +0300

    fixed word2vec

--- build.sbt
+++ build.sbt
@@ -3,14 +3,13 @@ name := "BD-A2"
 version := "0.1"
 
 scalaVersion := "2.11.8"
-val sparkVersion = "1.6.3"
+val sparkVersion = "2.4.4"
 
 resolvers += "snapshots-repo" at "https://oss.sonatype.org/content/repositories/snapshots"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  "org.apache.spark" %% "spark-streaming-twitter" % sparkVersion,
   "org.deeplearning4j" % "scalnet_2.11" % "0.9.2-SNAPSHOT",
   "org.apache.spark" %% "spark-mllib"%"2.4.0"
 )
--- src/main/scala/model/word2vec.scala
+++ src/main/scala/model/word2vec.scala
@@ -1,30 +1,42 @@
 package model
 
-
-import org.apache.spark.ml.feature.Word2Vec
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.RDD
+import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer, Word2Vec}
 import org.apache.spark.sql.{SQLContext, SparkSession}
 
 object word2vec {
   def main(args: Array[String]): Unit = {
-//    val conf = new SparkConf().setAppName("Word2vec")
-//    val sc = new SparkContext(conf)
+    //    val conf = new SparkConf().setAppName("Word2vec")
+    //    val sc = new SparkContext(conf)
     val spark = SparkSession.builder
       .master("local")
       .appName("Spark CSV Reader")
       .getOrCreate
-    val model = new Word2Vec()
-    model.setVectorSize(128)
-    model.setInputCol("SentimentText")
+
     val df = spark.read
       .format("csv")
       .option("header", "true") //first line in file has headers
-      .option("mode", "DROPMALFORMED")
       .load("dataset/train.csv")
     import spark.implicits._
-    val tmp = df.select("SentimentText").map(line => line.getString(0).split(" ")).toDF("SentimentText")
-    model.fit(tmp)
+    val model = new Word2Vec()
+    model.setMaxIter(5)
+    model.setVectorSize(128)
+    model.setInputCol("Tokens")
+    model.setOutputCol("result")
+
+    val tokenizer = new RegexTokenizer()
+      .setInputCol("SentimentText")
+      .setOutputCol("Tokens")
+      .setPattern("\\W+")
+      .setGaps(true)
+
+
+    val tmp = tokenizer.transform(df)
+    tmp.show(10)
+    val w2vmodel = model.fit(tmp)
+    val result = w2vmodel.transform(tmp)
+    result.select("result").take(3).foreach(println)
+    val ds = w2vmodel.findSynonyms("apple", 5).select("word")
+    ds.show(5)
   }
 
 }
