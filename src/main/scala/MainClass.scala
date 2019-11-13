322667a42aa1e7357cf5386739cd89010253ef6f
Mikhail Tkachenko <12102man@gmail.com>
2019-11-13 17:15:33 +0300

    +Stream read

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,27 +1,27 @@
-//import org.apache.spark._
-//import org.apache.spark.SparkContext._
-//import org.apache.spark.streaming._
-//import org.apache.spark.streaming.twitter._
-//import org.apache.spark.streaming.StreamingContext._
-//import org.apache.log4j.Level
-//import Utilities._
-//
-//
-//object MainClass{
-//  def main(args: Array[String]){
-//    // Configure Twitter credentials using twitter.txt
-//    setupTwitter()
-//    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
-//    setupLogging()
-//
-//    // Create a DStream from Twitter using our streaming context
-//    val tweets = TwitterUtils.createStream(ssc, None)
-//
-//    // Now extract the text of each status update into RDD's using map()
-//    val statuses = tweets.map(status => status.getText())
-//    statuses.print()
-//
-//    ssc.start()
-//    ssc.awaitTermination()
-//  }
-//}
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark
+import org.apache.spark.ml.classification.LogisticRegression
+
+object MainClass{
+  def main(args: Array[String]){
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+
+    // Words  reading
+    val lines = ssc.socketTextStream("10.91.66.168", 8989)
+    val words = lines.flatMap(_.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
