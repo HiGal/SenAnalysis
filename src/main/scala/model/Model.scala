22beab7c74e1b19e23ba7c910a6dea184b819eea
Farit Galeev <f.galeev@innopolis.ru>
2019-11-16 16:57:19 +0300

    Added Normalizer

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -1,26 +1,40 @@
 //package model
-//import org.apache.spark.ml.feature.Word2VecModel
+//import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 //import org.deeplearning4j.scalnet.layers.core.Dense
-//import org.deeplearning4j.scalnet.layers.embeddings.EmbeddingLayer
 //import org.deeplearning4j.scalnet.layers.recurrent.{Bidirectional, LSTM}
 //import org.deeplearning4j.scalnet.models.Sequential
 //import org.nd4j.linalg.activations.Activation
 //import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
-//import org.apache.spark.mllib.feature.Word2Vec
-//import org.nd4j.weightinit.WeightInit
-//import org.nd4j.weightinit.impl.XavierInitScheme
-//import org.apache.spark.sql.SparkSession
+//import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 //
 //class Model {
 //
-//  val seed: Int = 5
+//
+//  def train(dataset: DataFrame, maxIter: Int): Sequential =
+//  {
+//    val seed: Int = 5
+//
+//    val spark = SparkSession.builder
+//      .master("local")
+//      .appName("Model training")
+//      .getOrCreate
+//
+//    import spark.implicits._
 //
 //
-//  val model: Sequential = Sequential(rngSeed = seed)
+//    case class df_ds(features:Vector[Double],label:Int)
+//    val df = dataset.select("normedW2V", "Sentiment")
+//    val ds = df.as[df_ds]
 //
-//  model.add(Bidirectional(LSTM(128, 256, dropOut = 0.2)))
-//  model.add(Dense(512, 256,activation = Activation.RELU))
-//  model.add(Dense(2, 256, activation = Activation.SOFTMAX))
+//    val model: Sequential = Sequential(rngSeed = seed)
 //
-//  model.compile(LossFunction.NEGATIVELOGLIKELIHOOD)
+//    model.add(Bidirectional(LSTM(128, 256, dropOut = 0.2)))
+//    model.add(Dense(512, 256, activation = Activation.RELU))
+//    model.add(Dense(2, 256, activation = Activation.SOFTMAX))
+//
+//    model.compile(LossFunction.NEGATIVELOGLIKELIHOOD)
+//    model.fit(ds, maxIter, List(new ScoreIterationListener(100)))
+//    model.evaluate(dataset).f1()
+//
+//  }
 //}
