e25d09a251f9fe0c046211dec6119b851420d053
Vladik <qvviko@gmail.com>
2019-11-17 22:49:35 +0300

    - imports:

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,25 +1,14 @@
-import model.word2vec
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.log4j.Logger
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.log4j.Level
-import org.apache.spark
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
-import org.apache.spark.sql.{Column, Row, SaveMode, SparkSession}
-import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
-import org.apache.spark.ml.feature.{Normalizer, RegexTokenizer, StopWordsRemover, VectorAssembler}
-import org.apache.spark.sql.functions.regexp_replace
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-import org.apache.spark.sql.functions._
 import java.util.Calendar
+
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.classification.LogisticRegressionModel
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+import org.apache.spark.streaming._
 
 object MainClass {
   def main(args: Array[String]) {
-
-    Logger.getLogger("org").setLevel(Level.OFF)
-    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val spark = SparkSession.builder
       .master("local")
